5519ae5c8be6ff038cb54df8fafea83d466ab93b
Alyssa <000260219@student.baisd.net>
2026-02-12 08:55:54 -0500

    loading screen
    - loading screen has progress bar that completes in five seconds
    - immediately after loading, main menu scene is shown

--- src/main/java/com/ninelivesscholar/ninelivesscholar/HelloController.java
+++ src/main/java/com/ninelivesscholar/ninelivesscholar/HelloController.java
@@ -1,42 +1,53 @@
 package com.ninelivesscholar.ninelivesscholar;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ProgressBar;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
-public class HelloController implements Initializable {
+    public class HelloController implements Initializable {
 
-    @FXML
-    private ProgressBar pgBar;
+        @FXML
+        private ProgressBar pgBar;
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+        @Override
+        public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        pgBar.setProgress(0);
+            pgBar.setProgress(0);
+            Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(5), new KeyValue(pgBar.progressProperty(),1)));
+            timeline.setOnFinished(event -> {
+                try {
+                    mainMenuScene();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            timeline.play();
 
-        Timeline timeline = new Timeline(
-                new KeyFrame(Duration.seconds(5),
-                        new KeyValue(pgBar.progressProperty(), 1))
-        );
+        }
 
-        timeline.setOnFinished(event -> onProgressFinished());
+        public void mainMenuScene () throws IOException {
 
-        timeline.play();
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("main-menu.fxml")));
+            Stage stage = (Stage) pgBar.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+
+        }
+
     }
-
-    private void onProgressFinished() {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Finished");
-        alert.setHeaderText(null);
-        alert.setContentText("Loading Complete!");
-        alert.show();
-    }
-}
