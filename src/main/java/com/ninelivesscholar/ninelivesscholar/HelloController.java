63c0a4cfcbf90559eca6d790716060e74d8cb17a
Alyssa <000260219@student.baisd.net>
2026-02-11 21:08:18 -0500

    progress bar is made for loading screen

--- src/main/java/com/ninelivesscholar/ninelivesscholar/HelloController.java
+++ src/main/java/com/ninelivesscholar/ninelivesscholar/HelloController.java
@@ -1,14 +1,42 @@
 package com.ninelivesscholar.ninelivesscholar;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ProgressBar;
+import javafx.util.Duration;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ProgressBar pgBar;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        pgBar.setProgress(0);
+
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.seconds(5),
+                        new KeyValue(pgBar.progressProperty(), 1))
+        );
+
+        timeline.setOnFinished(event -> onProgressFinished());
+
+        timeline.play();
+    }
+
+    private void onProgressFinished() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Finished");
+        alert.setHeaderText(null);
+        alert.setContentText("Loading Complete!");
+        alert.show();
     }
 }
