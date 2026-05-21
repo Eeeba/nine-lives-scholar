0d7882e119c9de0719bc73f556fee2647aac15e8
Alyssa <000260219@student.baisd.net>
2026-05-21 07:49:02 -0400

    meowl go meow

--- src/main/java/com/ninelivesscholar/ninelivesscholar/MainMenuController.java
+++ src/main/java/com/ninelivesscholar/ninelivesscholar/MainMenuController.java
@@ -7,27 +7,13 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
 
 public class MainMenuController{
-
-    @FXML
-    private void openSoundSettings(MouseEvent event) throws IOException {
-
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("sound-settings.fxml")));
-        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        stage.setScene(new Scene(root));
-        stage.show();
-
-    }
 
     @FXML
     private void openPomodoroTimer(MouseEvent event) throws IOException {
@@ -40,30 +26,15 @@ public class MainMenuController{
     }
 
     @FXML
-    private ImageView profileImageView;
+    private void openStickerJournal(MouseEvent event) throws IOException {
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("journal.fxml")));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
 
     @FXML
     private TextField nameField;
-
-    @FXML
-    private void handleUploadImage() {
-        FileChooser fileChooser = new FileChooser();
-
-        fileChooser.getExtensionFilters().add(
-                new FileChooser.ExtensionFilter(
-                        "Image Files",
-                        "*.png",
-                        "*.jpg",
-                        "*.jpeg"
-                )
-        );
-
-        File file = fileChooser.showOpenDialog(null);
-
-        if (file != null) {
-            Image image = new Image(file.toURI().toString());
-            profileImageView.setImage(image);
-        }
-    }
 
 }
